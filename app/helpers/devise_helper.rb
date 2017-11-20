ba6bc89ee33b52bb3ab8e3c26e53e8deb4cb7fd2
marviorocha <marviorocha@marviorocha.com>
2017-11-20 12:29:30 -0200

    add fix layouts

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -2,11 +2,8 @@ module DeviseHelper
 
   def devise_notification!
 
-      if flash
-        render html:'<div class="alert alert-danger" >'.html_safe + flash["alert"] +'</div>'.html_safe
-      else
-         render html:'<div class="alert alert-success">'.html_safe + flash["notice"]  +'</div>'.html_safe
-      end
+    render 'messager/devisemessager'
+
   end
 
 
