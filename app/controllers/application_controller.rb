6fed4434d4d2a0a0d5ea814188b7f3749621f431
marviorocha <marviorocha@localhost.localdomain>
2018-02-02 15:42:44 -0200

    Update for deeple

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+
   before_action :authenticate_user!
   layout :layout_by_resource
   protect_from_forgery with: :exception
@@ -7,20 +8,17 @@ class ApplicationController < ActionController::Base
   client = OneSignal::Client.new(auth_token: 'YmFmNjdlM2QtY2RjNS00MjlhLWIxYTItYTkxMTYxYWJhNTIy', app_id: 'f25ce7ac-9737-4b46-af9a-42c20444cf12')
   client.players.all
 
-
-
-
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :lastname])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :lastname])
+    
+
   end
 
 
   private
-
-
 
   def layout_by_resource
      if user_signed_in? == false
@@ -29,7 +27,7 @@ class ApplicationController < ActionController::Base
        "application"
      end
 
-   end
+  end
 
 
 end
