a4cac3047ad51f593eca4c0acdb9317b5ecade79
marviorocha <marviorocha@marviorocha.com>
2018-02-10 02:51:39 -0200

    Add form create users with curses_ids

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,14 +8,6 @@ class ApplicationController < ActionController::Base
   client = OneSignal::Client.new(auth_token: 'YmFmNjdlM2QtY2RjNS00MjlhLWIxYTItYTkxMTYxYWJhNTIy', app_id: 'f25ce7ac-9737-4b46-af9a-42c20444cf12')
   client.players.all
 
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :lastname])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :lastname, curse_ids: [] ])
-
-
-  end
 
 
   private
@@ -28,6 +20,13 @@ class ApplicationController < ActionController::Base
      end
 
   end
+
+  def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :lastname, :curse_ids ])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :lastname, :curse_ids ])
+
+  end
+
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
 
 
 
-devise_for :users
+devise_for :users, controllers: { sessions: 'users/sessions' }
 
-get '/users/sign_up/:id', to: 'users#new', as: 'user_signup'
+get '/users/sign_up/:id', to: 'users#new', as: 'user_new'
 
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
