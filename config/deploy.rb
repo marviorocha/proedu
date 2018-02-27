89fce65b04be4ef9a6cb4bd015f4382c21cce202
marviorocha <marviorocha@marviorocha.com>
2018-02-27 19:18:40 -0300

    fix editor

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.10.1"
 
 set :application, "proedu"
-set :repo_url, "https://github.com/marviorocha/proedu.git"
+set :repo_url, "git@github.com:marviorocha/proedu.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
