1cb1e5514998bdd8e0ef755c025b56a22abd0519
Marvio Rocha <marviorocha@marviorocha.com>
2019-09-18 20:57:42 -0300

    Add new logout menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,12 +1,4 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
-// vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
+ 
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 
@@ -17,6 +9,7 @@
 //= require extras.1.1.0.min
 //= require shards-dashboards.1.1.0.min
 //= require app/app-blog-overview.1.1.0
-
+//= require rails-ujs
+//= require turbolinks
 
  
