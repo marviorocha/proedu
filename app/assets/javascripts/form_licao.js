af2a6eae0ba807b8ebc9f80deb7d629fa5b6e36e
Marvio Rocha <marviorocha@marviorocha.com>
2019-09-30 00:15:56 -0300

    Add new forms

--- app/assets/javascripts/form_licao.js
+++ app/assets/javascripts/form_licao.js
@@ -1,20 +1,35 @@
 $(document).ready(function(){
 
+    $('.publish-show').hide();
     $("#jobs").click(function(){
+      $('.publish-show').toggle(10)
+    });
+
+    
+    $('#content_publish_on').on('input',function(){
        
-        $('.publish-show').toggle(10)
+        $('#content_publish_on_display').text($(this).val());
+
+    })
+
+    $('#content_publish').on('option', function(){
+       
+        $('#content_publish_display').text($(this).val());
+
     })
 
 });
 
 // Form datapick
-
+ 
  
 $(function() {
-
+   
     $('.calendar').flatpickr({
         minDate: "today",
         dateFormat: "d-m-Y",
+        allowInput: true
+        
     }
 
        
