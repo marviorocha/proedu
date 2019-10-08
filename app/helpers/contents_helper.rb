71e91e8dde9cfe25af46c146e068c0c821bb2f83
Marvio Rocha <marviorocha@marviorocha.com>
2019-10-08 01:03:19 -0300

    Add video

--- app/helpers/contents_helper.rb
+++ app/helpers/contents_helper.rb
@@ -1,13 +1,6 @@
 module ContentsHelper
 
 
-    def show_video
-
-        text = "A regular expression is a sequence of characters that define a search pattern."
-
-        text.match(/^A/)
-        
-    end
-    
+  
 
 end
