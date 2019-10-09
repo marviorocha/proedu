550474b3333a0742d2f7a25d5803b9e1b7d72f0f
Marvio Rocha <marviorocha@marviorocha.com>
2019-10-08 22:08:28 -0300

    Add player modal

--- app/assets/javascripts/form_licao.js
+++ app/assets/javascripts/form_licao.js
@@ -1,35 +1,38 @@
 $(document).ready(function(){
-
-    $('.publish-show').hide();
-    $("#jobs").click(function(){
-      $('.publish-show').toggle(10)
-    });
-
-    
-    $('#content_publish_on').on('input',function(){
-       
-        $('#content_publish_on_display').text($(this).val());
-
-    })
-
- 
-
+	
+	$('.publish-show').hide();
+	$("#jobs").click(function(){
+		$('.publish-show').toggle(10)
+	});
+		
+	$('#content_publish_on').on('input',function(){
+		$('#content_publish_on_display').text($(this).val());
+	})
+		
 });
 
 // Form datapick
- 
- 
+
 $(function() {
-   
-    $('.calendar').flatpickr({
-        minDate: "today",
-        dateFormat: "d/m/Y",
-        allowInput: true
-        
-    }
+	$('.calendar').flatpickr({
+		minDate: "today",
+		dateFormat: "d/m/Y",
+		allowInput: true
+		
+	}
+	);
+});
 
-       
-    );
+// Animated Modal 01
 
- 
-  });
+$(function(){
+
+	$("#demo01").animatedModal({
+		animatedIn:'slideInUp',
+		animatedOut:'bounceOutDown',
+		color:'#212529',
+		overflow: 'auto'
+	 
+	});
+
+});
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,7 +5,7 @@ require("@rails/actiontext")
 require("flatpickr/dist/flatpickr.css")
 require("plyr/dist/plyr.css")
 import flatpickr from 'flatpickr'
+import animatedmodal from 'animatedmodal'
 import Plyr from 'plyr';
-
 const player = new Plyr('#player');
 
