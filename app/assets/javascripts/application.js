d20d727a456d9532cee2466abaa6a996eb9bc401
Marvio Rocha <marviorocha@marviorocha.com>
2019-11-11 00:49:28 -0300

    jquery fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,7 @@
-//= require jquery3
-//= require rails-ujs
 //= require clipboard/dist/clipboard.min.js
 //= require popper.js/dist/popper
+//= require jquery3
+//= require rails-ujs
 //= require bootstrap/dist/js/bootstrap.min
 //= require chart.js/dist/Chart.min.js
 //= require extras.1.1.0.min
@@ -16,5 +16,3 @@ var clipboard = new ClipboardJS('.btn')
 $("#selectAll").select_all({
 	infinite_scroll_select: true
 });
-
-
